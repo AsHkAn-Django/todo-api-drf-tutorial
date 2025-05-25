2d2715887fefdbee3909adee474bca9be929acaa
AsHkaN <ashkan_ahrari@yahoo.com>
2025-05-25 18:24:42 +0300

    viewset added instead of generic

--- myApp/urls.py
+++ myApp/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import SimpleRouter
 
 
-urlpatterns = [
-    path('', views.TaskList.as_view()),
-    path('edit/<int:pk>/', views.TaskDetail.as_view()),
-]
+router = SimpleRouter()
+router.register('', views.TaskViewSet, basename='tasks')
+
+urlpatterns = router.urls
+
+
+# urlpatterns = [
+#     path('', views.TaskList.as_view()),
+#     path('edit/<int:pk>/', views.TaskDetail.as_view()),
+# ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,15 +1,24 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .serializers import TaskSerializers
 from .models import Task
 from .permissions import AuthorOnly
 
 
-class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+
+class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = (AuthorOnly,)
     queryset = Task.objects.all()
     serializer_class = TaskSerializers
+
+
+
+
+# class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+#     permission_classes = (AuthorOnly,)
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializers
     
     
-class TaskList(generics.ListCreateAPIView):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializers
+# class TaskList(generics.ListCreateAPIView):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializers
