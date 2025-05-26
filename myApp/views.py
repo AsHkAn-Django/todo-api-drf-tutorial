bb8a4b2b8a1f9b753b3c13c293add6fab9ce70d5
AsHkaN <ashkan_ahrari@yahoo.com>
2025-05-26 18:00:08 +0300

    filter base on completed or title added

--- myApp/views.py
+++ myApp/views.py
@@ -7,9 +7,24 @@ from .permissions import AuthorOnly
 
 class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = (AuthorOnly,)
-    queryset = Task.objects.all()
     serializer_class = TaskSerializers
 
+    def get_queryset(self):
+        queryset = Task.objects.all()
+        # for getting the querry parameter in drf use `self.request.query_params.get` instead of `self.request.GET.get()`
+        # `self.request.GET.get()` still works in drf but not recommended
+        completed = self.request.query_params.get('completed')
+        title = self.request.query_params.get('title')
+        
+        # We check if the completed parameter is not empty(we don't say `if completed` because in that way we check the boolean)
+        if completed is not None:
+            # if the parameter is in this list (TRUE or 1)
+            queryset = queryset.filter(completed=completed.lower() in ['true', '1'])
+        
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        
+        return queryset
 
 
 
